a757b1067a3f2be49c1f8b38779afdeabb70be64
Natalie Moroz <nataliemoroz2229@gmail.com>
2026-05-05 00:57:32 +0300

    add section news

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -28,3 +28,46 @@ $(document).ready(function () {
     ],
   });
 });
+
+
+$(document).ready(function () {
+  $(".slider_2").slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 3,
+    speed: 1400,
+    dots: true,
+    arrows: true,
+    infinite: true,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          autoplaySpeed: 3000,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          autoplaySpeed: 2000,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          autoplaySpeed: 1000,
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
+
+
